6ba2d3d2fd20921edec6fe50ddcf83eba415d0fd
ipshitaRB <ipshitarburman@gmail.com>
2020-05-14 15:58:15 -0400

    Add navigations from MainFragment

--- app/src/main/java/com/droidrbi/mywallet/MainFragment.kt
+++ app/src/main/java/com/droidrbi/mywallet/MainFragment.kt
@@ -1,15 +1,20 @@
 package com.droidrbi.mywallet
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 
 /**
  * A simple [Fragment] subclass.
  */
-class MainFragment : Fragment() {
+class MainFragment : Fragment(), View.OnClickListener {
+
+    lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,6 +22,23 @@ class MainFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        view.findViewById<Button>(R.id.view_balance_btn).setOnClickListener(this)
+        view.findViewById<Button>(R.id.send_money_btn).setOnClickListener(this)
+        view.findViewById<Button>(R.id.view_transactions_btn).setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v!!.id) {
+            R.id.view_transactions_btn -> navController.navigate(R.id.action_mainFragment_to_viewTransactionFragment)
+            R.id.send_money_btn -> navController.navigate(R.id.action_mainFragment_to_chooseRecipientFragment)
+            R.id.view_balance_btn -> navController.navigate(R.id.action_mainFragment_to_viewBalanceFragment)
+        }
+
     }
 
 }
