1543426bc55e0ca7336b144764115b083745dc73
ipshitaRB <ipshitarburman@gmail.com>
2020-05-15 12:25:48 -0400

    Pass recipient and amount between fragments

--- app/src/main/java/com/droidrbi/mywallet/ChooseRecipientFragment.kt
+++ app/src/main/java/com/droidrbi/mywallet/ChooseRecipientFragment.kt
@@ -1,13 +1,16 @@
 package com.droidrbi.mywallet
 
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_choose_recipient.*
 
 /**
  * A simple [Fragment] subclass.
@@ -33,7 +36,14 @@ class ChooseRecipientFragment : Fragment(), View.OnClickListener {
 
     override fun onClick(v: View?) {
         when (v!!.id) {
-            R.id.next_btn -> navController.navigate(R.id.action_chooseRecipientFragment_to_specifyAmountFragment)
+            R.id.next_btn ->
+                if (!TextUtils.isEmpty(input_recipient.text.toString())) {
+                    val bundle = bundleOf("recipient" to input_recipient.text.toString())
+                    navController.navigate(
+                        R.id.action_chooseRecipientFragment_to_specifyAmountFragment,
+                        bundle
+                    )
+                }
             R.id.cancel_btn -> requireActivity().onBackPressed()
         }
     }
--- app/src/main/java/com/droidrbi/mywallet/SpecifyAmountFragment.kt
+++ app/src/main/java/com/droidrbi/mywallet/SpecifyAmountFragment.kt
@@ -1,13 +1,19 @@
 package com.droidrbi.mywallet
 
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_specify_amount.*
+import java.math.BigDecimal
 
 /**
  * A simple [Fragment] subclass.
@@ -15,6 +21,12 @@ import androidx.navigation.Navigation
 class SpecifyAmountFragment : Fragment(), View.OnClickListener {
 
     lateinit var navController: NavController
+    lateinit var recipient: String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        recipient = requireArguments().getString("recipient")!!
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,11 +42,27 @@ class SpecifyAmountFragment : Fragment(), View.OnClickListener {
         navController = Navigation.findNavController(view)
         view.findViewById<Button>(R.id.send_btn).setOnClickListener(this)
         view.findViewById<Button>(R.id.cancel_btn).setOnClickListener(this)
+        val message = "Sending money to  $recipient"
+        view.findViewById<TextView>(R.id.recipient).text = message
     }
 
     override fun onClick(v: View?) {
         when (v!!.id) {
-            R.id.send_btn -> navController.navigate(R.id.action_specifyAmountFragment_to_confirmationFragment)
+            R.id.send_btn ->
+                if (!TextUtils.isEmpty(input_amount.toString())) {
+                    val amount = Money(BigDecimal(input_amount.text.toString()))
+                    val bundle = bundleOf(
+                        "recipient" to recipient,
+                        "amount" to amount
+                    )
+                    navController.navigate(
+                        R.id.action_specifyAmountFragment_to_confirmationFragment,
+                        bundle
+                    )
+                } else {
+                    Toast.makeText(activity, "Enter an amount", Toast.LENGTH_LONG).show()
+                }
+
             R.id.cancel_btn -> requireActivity().onBackPressed()
 
 
